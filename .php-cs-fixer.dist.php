1a95084182b4b02f83974d49c20fc72c221aa479
Oleksandr Tishchenko <dev@smartresponsor.com>
2026-09-14 12:37:06 -0500

    build: unify php cs fixer configuration

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,26 +2,4 @@
 
 declare(strict_types=1);
 
-$finder = PhpCsFixer\Finder::create()
-    ->in([__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/config', __DIR__ . '/tools'])
-    ->name('*.php')
-    ->notPath('reference.php');
-
-return (new PhpCsFixer\Config())
-    ->setRiskyAllowed(true)
-    ->setFinder($finder)
-    ->setRules([
-        '@PSR12' => true,
-        'array_syntax' => ['syntax' => 'short'],
-        'declare_strict_types' => true,
-        'ordered_imports' => true,
-        'no_unused_imports' => true,
-        'single_quote' => true,
-        'trailing_comma_in_multiline' => ['elements' => ['arrays']],
-        'phpdoc_to_comment' => false,
-        'phpdoc_summary' => false,
-        'phpdoc_align' => false,
-        'phpdoc_separation' => false,
-        'phpdoc_trim' => false,
-        'no_superfluous_phpdoc_tags' => false,
-    ]);
+return require __DIR__.'/.php-cs-fixer.php';
